c481d5096a178e5dd95cc2a1d6fb7c1ec630b906
s309923 <s309923@studenti.polito.it>
2024-02-06 18:40:38 +0100

    refactor e new db

--- REST APIs Implementation/service/DelegationsService.js
+++ REST APIs Implementation/service/DelegationsService.js
@@ -19,51 +19,51 @@ exports.issueDelegation = function(invitation, reviewerId, userId) {
               } else {
                   const sql4 = "SELECT * FROM reviews r WHERE filmId = ? AND reviewerID = ?"
                   db.get(sql4, [invitation.filmId, invitation.delegatedId], (err, riga) => {
-                    if(err) reject(err);
-                    else if(riga){
-                        reject(408);
-                    }else{
-                        const sql1 = "SELECT f.owner as owner, r.filmId as fid, r.reviewerId as rid, r.completed, r.reviewDate, r.rating, r.review FROM reviews r, films f WHERE f.id=filmId AND filmId = ? AND reviewerId = ?";
-                  db.all(sql1, [invitation.filmId, reviewerId], (err, rows) => {
-                      if (err) {
-                          reject(err);
-                      } else if (invitation.delegatedId == userId) {
-                          reject(413);
-                      } else if (rows.length === 0) {
-                          reject(404);
-                      } else if (userId != rows[0].rid) {
-                          reject(404);
-                      } else if (invitation.delegatedId == rows[0].owner) {
-                          reject(412);
-                      } else if (reviewerId != rows[0].rid) {
-                          reject(403);
-                      } else if (rows[0].completed == 1) {
-                          reject(405);
+                      if (err) reject(err);
+                      else if (riga) {
+                          reject(408);
                       } else {
-                          var temp = new Delegation(rows[0].fid, rows[0].rid, 0, rows[0].completed, rows[0].reviewDate, rows[0].rating, rows[0].review);
-                          var sql2 = 'SELECT * FROM users WHERE id = ?';
-                          db.all(sql2, invitation.delegatedId, async function (err, rows) {
+                          const sql1 = "SELECT f.owner as owner, r.filmId as fid, r.reviewerId as rid, r.completed, r.reviewDate, r.rating, r.review FROM reviews r, films f WHERE f.id=filmId AND filmId = ? AND reviewerId = ?";
+                          db.all(sql1, [invitation.filmId, reviewerId], (err, rows) => {
                               if (err) {
                                   reject(err);
-                              } else if (rows.length !== 1) {
-                                  reject(409);
+                              } else if (invitation.delegatedId == userId) {
+                                  reject(413);
+                              } else if (rows.length === 0) {
+                                  reject(404);
+                              } else if (userId != rows[0].rid) {
+                                  reject(404);
+                              } else if (invitation.delegatedId == rows[0].owner) {
+                                  reject(412);
+                              } else if (reviewerId != rows[0].rid) {
+                                  reject(403);
+                              } else if (rows[0].completed == 1) {
+                                  reject(405);
                               } else {
-                                  temp.delegatedId = rows[0].id;
-                                  const sql3 = 'INSERT INTO delegations(filmId, reviewerId, delegatedId, completed, reviewDate, rating, review) VALUES(?,?,?,?,?,?,?)';
-                                  db.run(sql3, [temp.filmId, temp.reviewerId, temp.delegatedId, temp.completed, temp.reviewDate, temp.rating, temp.review], function (err) {
+                                  var temp = new Delegation(rows[0].fid, rows[0].rid, 0, rows[0].completed, rows[0].reviewDate, rows[0].rating, rows[0].review);
+                                  var sql2 = 'SELECT * FROM users WHERE id = ?';
+                                  db.all(sql2, invitation.delegatedId, async function (err, rows) {
                                       if (err) {
-                                          reject('500');
+                                          reject(err);
+                                      } else if (rows.length !== 1) {
+                                          reject(409);
                                       } else {
-                                          var createdDelegation = new Delegation(temp.filmId, temp.reviewerId, temp.delegatedId, temp.completed, temp.reviewDate, temp.rating, temp.review);
-                                          resolve(createdDelegation);
+                                          temp.delegatedId = rows[0].id;
+                                          const sql3 = 'INSERT INTO delegations(filmId, reviewerId, delegatedId, completed, reviewDate, rating, review) VALUES(?,?,?,?,?,?,?)';
+                                          db.run(sql3, [temp.filmId, temp.reviewerId, temp.delegatedId, temp.completed, temp.reviewDate, temp.rating, temp.review], function (err) {
+                                              if (err) {
+                                                  reject('500');
+                                              } else {
+                                                  var createdDelegation = new Delegation(temp.filmId, temp.reviewerId, temp.delegatedId, temp.completed, temp.reviewDate, temp.rating, temp.review);
+                                                  resolve(createdDelegation);
+                                              }
+                                          });
                                       }
                                   });
                               }
                           });
-                      }
-                  });
 
-                    }
+                      }
                   })
               }
           });
